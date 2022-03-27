350134c4a684a537156c4d55076fd0cad17504b1
Sergey Chepukhin <s.chepukhin@iq-adv.ru>
2022-03-27 19:44:35 +0500

    config | ⚙️ add webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.ts",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist/"),
+    library: {
+      name: "awesome-finance-react",
+      type: "umd",
+    },
+    clean: true,
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  externals: {
+    react: "react",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        use: ["ts-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
